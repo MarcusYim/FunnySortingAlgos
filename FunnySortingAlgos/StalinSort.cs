6de335d19338fefec806d48e52357044d85f33df
MarcusYim <marcusyim2004@gmail.com>
2022-02-28 10:14:59 -0800

    Add project files.

---
+++ FunnySortingAlgos/StalinSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunnySortingAlgos
+{
+    class StalinSort
+    {
+        public void stalinSort(int[] arr)
+        {
+            Dictionary<int, int> dict = arr.ToDictionary(item => item, item => item);
+            Dictionary<int, int> ret = new Dictionary<int, int>();
+
+            for (int i = 1; i < dict.Count; i++)
+            {
+                ret[i] = dict[i];
+
+                if (dict[i - 1] >= dict[1])
+                {
+                    ret[i - 1] = dict[i - 1];
+                }
+            }
+
+            foreach (int i in dict.Keys)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 0, 3, 2, 5, 1, 9, 10 };
+
+            StalinSort p = new StalinSort();
+            p.stalinSort(arr);
+        }
+    }
+}
---
+++ FunnySortingAlgos/WaitSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunnySortingAlgos
+{
+    class WaitSort
+    {
+        public void waitAndPrint(int val)
+        {
+            Thread.Sleep(val * 1000);
+            Console.WriteLine(val);
+        }
+
+        private void waitSort(int[] arr)
+        {
+            var dict = arr.ToDictionary(item => item, item => item);
+            Parallel.ForEach(dict, val => waitAndPrint(val.Key));
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 0, 3, 2, 5, 1, 9, 10 };
+
+            WaitSort p = new WaitSort();
+            p.waitSort(arr);
+        }
+    }
+}
